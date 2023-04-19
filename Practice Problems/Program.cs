cdca3db7307b9f423fa0beb4bee31f75ed071295
Sushil <skshinde2028@gmail.com>
2023-04-19 15:33:33 +0530

    [Sushil] Welcome message on main branch

---
+++ Practice Problems/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the practice problems");
+        }
+    }
+}
