ac148525112dc2e747b844a91c3d13d7cd423ac3
Sushil <skshinde2028@gmail.com>
2023-04-19 16:02:32 +0530

    [Sushil] String Reverse

--- Practice Problems/Program.cs
+++ Practice Problems/Program.cs
@@ -11,6 +11,19 @@ namespace Practice_Problems
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the practice problems");
+            Console.WriteLine("Choose the option from below.\n");
+            Console.WriteLine("1. Reverse a string\n ");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch(option)
+            {
+                case 1:
+                    ReverseString.FindStringReverse();
+                    break;
+                default:
+                    Console.WriteLine("Please choose the valid option");
+                    break;
+            }    
         }
     }
 }
---
+++ Practice Problems/ReverseString.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Problems
+{
+    internal class ReverseString
+    {
+        public static void FindStringReverse()
+        {
+            Console.WriteLine("Enter a string.");
+            string s = Console.ReadLine();
+            Console.WriteLine("Before Reverse: "+s);
+            string reverse = "";
+            int l = s.Length - 1;
+            while (l >= 0)
+            {
+                reverse = reverse + s[l];
+                l--;
+            }
+            Console.WriteLine("After reverse: "+reverse);
+        }
+    }
+}
